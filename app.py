fa50c75e18f8a93f6cfb4f0d34c4c41e72412df0
Sadettin Furkan Çalış <b2200356030@cs.hacettepe.edu.tr>
2024-04-23 18:15:28 +0300

    signup created with database integration

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,request,jsonify,redirect,url_for
 from user import user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.register_blueprint(user,url_prefix='/user')
 
 @app.route('/')
--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, Blueprint
+from flask import Flask, render_template, request, jsonify, redirect, url_for, Blueprint, flash
 import psycopg2
 import yaml
 from werkzeug.security import generate_password_hash
@@ -9,16 +9,26 @@ user = Blueprint('user', __name__ , static_folder='static', template_folder='tem
 @user.route('/signin' , methods=['GET','POST'])
 def signin():
     if request.method == 'POST':
-
         return redirect(url_for("home"))
     else:
-
         return render_template('signin.html')
 
-@user.route('/signup')
+@user.route('/signup' , methods=['GET','POST'])
 def signup():
     if request.method == 'POST':
+        data = request.form
+        username = data['username']
+        mail = data['mail']
+        password = data['password']
+        hashed_password = generate_password_hash(password)
+        conn = RunFirstSettings.create_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO users (username, password,email) VALUES (%s, %s,%s)', (username, hashed_password, mail))
+        conn.commit()
+        conn.close()
 
-        return redirect(url_for("home"))
+        flash("You have successfully registered", "signup-success")
+        return redirect(url_for("user.signin"))
     else:
+        print(1123123)
         return render_template('signup.html')
