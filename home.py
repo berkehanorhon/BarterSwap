438766cbe7975bfebc419b6f52617ee7d39a172c
Sadettin Furkan Çalış <b2200356030@cs.hacettepe.edu.tr>
2024-05-11 21:40:53 +0300

    error in search solved

--- home.py
+++ home.py
@@ -27,14 +27,27 @@ def home(page):
 
     return render_template('home.html', items=items, total_pages=total_pages+1, current_page=page)
 
-@home_bp.route("/search", methods=['GET'])
-def search():
+@home_bp.route("/search", defaults={'page': 1}, methods=['GET'])
+@home_bp.route("/search/<int:page>", methods=['GET'])
+def search(page):
+    per_page = 10  # Change this as per your requirement
+    offset = (page - 1) * per_page
+
     query = request.args.get('query')
-    print(query)
+
     conn = RunFirstSettings.create_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM items WHERE title LIKE %s', ('%' + query + '%',))
+
+    cursor.execute('SELECT COUNT(*) FROM items WHERE title LIKE %s', ('%' + query + '%',))
+    total_items = cursor.fetchone()[0]
+    total_pages = math.ceil(total_items / per_page)
+
+    if page <= 0 or page > total_pages:
+        return render_template('404.html')
+
+    cursor.execute('SELECT * FROM items WHERE title LIKE %s ORDER BY item_id LIMIT %s OFFSET %s', ('%' + query + '%', per_page, offset))
     items = cursor.fetchall()
+
     conn.close()
 
-    return render_template('home.html', items=items, search=query)
+    return render_template('home.html', items=items, search=query, total_pages=total_pages+1, current_page=page)
