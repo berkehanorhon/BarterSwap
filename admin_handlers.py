aa29e9b83b1fa69a27b4e546b0ce90c1093cc00b
Sadettin Furkan Çalış <b2200356030@cs.hacettepe.edu.tr>
2024-05-20 23:11:59 +0300

    to merge

--- admin_handlers.py
+++ admin_handlers.py
@@ -1,3 +1,5 @@
+import math
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask import Blueprint
 from werkzeug.security import check_password_hash
@@ -10,13 +12,31 @@ admin_handlers = Blueprint('admin_handlers', __name__, static_folder='static', t
 def home():
     return render_template('admin/adminhome.html')
 
-@admin_handlers.route('/view_users')
-def view_users():
-    # Get all users from the database
-    # ...
+ITEMS_PER_PAGE = 10
 
-    # Render a template with the users
-    return render_template('view_users.html', users=users)
+@admin_handlers.route("/view_users/", defaults={'page': 1}, methods=['GET'])
+@admin_handlers.route("/view_users/<int:page>", methods=['GET'])
+def view_users(page):
+    search_query = request.args.get('search', '')
+    per_page = 10  # Change this as per your requirement
+    offset = (page - 1) * per_page
+
+    conn = RunFirstSettings.create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT COUNT(*) FROM users WHERE username LIKE %s', ('%' + search_query + '%',))
+    total_items = cursor.fetchone()[0]
+    total_pages = math.ceil(total_items / per_page)
+
+    if page <= 0 or page > total_pages:
+        return render_template('404.html')
+
+    cursor.execute('SELECT * FROM users WHERE username LIKE %s ORDER BY user_id LIMIT %s OFFSET %s', ('%' + search_query + '%', per_page, offset))
+    users = cursor.fetchall()
+
+    conn.close()
+
+    return render_template('admin/view_users.html', users=users, search=search_query, total_pages=total_pages+1, current_page=page)
 
 @admin_handlers.route('/view_items')
 def view_items():
